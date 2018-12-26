1c54e4d14313f7385ec638cf415d6256212635b9
jlbeidler <35079372+jlbeidler@users.noreply.github.com>
2018-12-26 14:57:34 -0500

    Update hourly_domain.py

--- emisqa/runtypes/hourly_domain.py
+++ emisqa/runtypes/hourly_domain.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 from emisqa.data_file import DataFile
 from emisqa.species_array import SpeciesArray
+import numpy as np
 
 def get_spec(species_name, opts):
     '''
@@ -14,7 +15,7 @@ def get_spec(species_name, opts):
         print('Creating domain total for species: %s' %species_name)
     if species_name not in file1.species_list and self.ignore_spec:
         print('WARNING: The species %s does not exist in the file %s.  Skipping.' %(species_name, file1))
-        DV = 0
+        DV = SpeciesArray(np.zeros([24,1,1]), species_name)
     else:
         DV = SpeciesArray(file1.dump_val(species_name, opts.all_hours, opts.grid, opts.ignore_spec, 
           opts.inln, opts.interpolate, opts.layer, opts.stacks), species_name)
