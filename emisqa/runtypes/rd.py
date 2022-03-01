34790bbf1e06188d8bef52ea8acbf6b9081e5bbf
James Beidler <james.beidler@gmail.com>
2022-03-01 11:39:42 -0500

    Further fix to raw dump

--- emisqa/runtypes/rd.py
+++ emisqa/runtypes/rd.py
@@ -1,5 +1,4 @@
 from __future__ import print_function
-from sys import exit
 from emisqa.data_file import DataFile
 import numpy as np
 from emisqa.species_array import SpeciesArray
@@ -9,14 +8,13 @@ def get_spec(species_name, opts):
     Dumps all summed column and rows for the day.
     '''
     file1 = DataFile(opts.file_name, opts.verbosity, opts.informat, opts.ptsr, opts.zip_dict)
-    print('Writing Domain...')
     if opts.verbosity: 
-        print('Creating raw dump for species: %s' %species_name)
+        print('Creating summed dump for species: %s' %species_name)
     if species_name not in file1.species_list and opts.ignore_spec:
         print('WARNING: The species %s does not exist in the file %s.  Skipping.' %(species_name, file1))
-        DV = SpeciesArray(np.zeros([24,opts.grid.NROWS,opts.grid.NCOLS]), species_name)
+        DV = 0
     else:
-        DV = SpeciesArray(file1.dump_val(species_name, opts.all_hours, opts.grid, opts.ignore_spec, 
+        DV = SpeciesArray(file1.sum_val(species_name, opts.all_hours, opts.grid, opts.ignore_spec, 
           opts.inln, opts.interpolate, opts.layer, opts.stacks), species_name)
     return (species_name, DV)
 
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="emisqa",
-    version="0.1.4",
+    version="0.1.5",
     packages=find_packages(),
     scripts = ['bin/emisqa',],
     package_data = {'emisqa': ['ancillary/*','ancillary/2016/*']}, 
