c66985a1c0ef5e79fe4060264886b1c25367c71e
James Beidler <35079372+jlbeidler@users.noreply.github.com>
2023-04-27 15:42:31 -0400

    move to distutils, explicit versions for modules

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup, find_packages
+from distutils.core import setup
 setup(
     name="emisqa",
-    version="0.2",
-    packages=find_packages(),
+    version="0.3.1",
+    packages=['emisqa','emisqa.camx','emisqa.cmaq','emisqa.csv','emisqa.dataout','emisqa.dateloop','emisqa.inline','emisqa.runtypes'],
     scripts = ['bin/emisqa',],
     package_data = {'emisqa': ['ancillary/*','ancillary/2016/*']}, 
     python_requires='>3.5',
-    setup_requires=['numpy>=1.12','netCDF4>=1.2.9','pandas>=0.20'],
+    setup_requires=['numpy>=1.12,<=1.24.3','netCDF4>=1.2.9,<=1.5.8','pandas>=0.20,<1','fauxioapi>=0.1.5'],
     author_email='james.beidler@gmail.com'
 )
